c7482022900bf40b4cf719bfd020464dac5f47cf
qaMilord <rustamw888@gmail.com>
2022-08-23 02:49:35 +0300

    fix code

--- tests/test_demoqa_check_form.py
+++ tests/test_demoqa_check_form.py
@@ -1,3 +1,5 @@
+import allure
+
 from lesson.model import application_manager
 from lesson.model.data.data import Student
 
@@ -21,38 +23,42 @@ student = Student(
 )
 
 
+@allure.title("Successful fill form")
 def test_submit_form():
     # GIVEN
-    application_manager.opened_practice_form()
+    with allure.step("Open registrations form"):
+        application_manager.opened_practice_form()
 
     # WHEN
-    (application_manager.form
-        .set_first_name(student.first_name)
-        .set_last_name(student.last_name)
-        .set_email(student.email)
-        .set_gender(student.gender)
-        .set_mobile_number(student.mobile_number)
-        .set_birth_date(student.birth_year, student.birth_month, student.birth_day)
-        .set_subjects(student.subjects)
-        .subjects_should_have(student.subjects)
-        .set_hobbies(student.hobbies)
-        .set_photo(student.photo)
-        .set_current_address(student.current_address)
-        .set_state(student.state)
-        .set_city(student.city)
-        .submit()
-    )
+    with allure.step("Fill form"):
+        (application_manager.form
+            .set_first_name(student.first_name)
+            .set_last_name(student.last_name)
+            .set_email(student.email)
+            .set_gender(student.gender)
+            .set_mobile_number(student.mobile_number)
+            .set_birth_date(student.birth_year, student.birth_month, student.birth_day)
+            .set_subjects(student.subjects)
+            .subjects_should_have(student.subjects)
+            .set_hobbies(student.hobbies)
+            .set_photo(student.photo)
+            .set_current_address(student.current_address)
+            .set_state(student.state)
+            .set_city(student.city)
+            .submit()
+        )
 
     # THEN
-    application_manager.result.verify_sent_data(
-        student.full_name(student.first_name, student.last_name),
-        student.email,
-        student.gender,
-        student.mobile_number,
-        student.date_of_birth,
-        student.subjects,
-        student.hobbies,
-        student.photo,
-        student.current_address,
-        student.state_and_city(student.state, student.city)
-    )
+    with allure.step("Check form results"):
+        application_manager.result.verify_sent_data(
+            student.full_name(student.first_name, student.last_name),
+            student.email,
+            student.gender,
+            student.mobile_number,
+            student.date_of_birth,
+            student.subjects,
+            student.hobbies,
+            student.photo,
+            student.current_address,
+            student.state_and_city(student.state, student.city)
+        )
