742e4655e5346f5d512bd6371e276bcd3acdb4c3
qaMilord <rustamw888@gmail.com>
2022-08-23 22:11:49 +0300

    fix code

--- lesson/model/application_manager.py
+++ lesson/model/application_manager.py
@@ -1,3 +1,4 @@
+from selene import have, command
 from selene.support.shared import browser
 
 from lesson.model.pages.student_registration_page import StudentRegistrationForm, ModalDialogSubmittingForm
@@ -8,3 +9,6 @@ result = ModalDialogSubmittingForm()
 
 def opened_practice_form():
     browser.open('/automation-practice-form')
+    browser.all('[id^=google_ads][id$=container__],[id$=Advertisement]').with_(timeout=10)\
+        .should(have.size_less_than_or_equal(4))\
+        .perform(command.js.remove)
